2b6375bbeacd818e32b61e6effef8d160f6b6417
TitTolkachev <tit.tolk07022003@gmail.com>
2023-02-21 14:32:25 +0700

    Сверстал модальное окно для добавления админом предмета

--- src/app/pages/admin/audiences/audiences-page/audiences-page.component.ts
+++ src/app/pages/admin/audiences/audiences-page/audiences-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-audiences-page',
   templateUrl: './audiences-page.component.html',
-  styleUrls: ['../../css/admin-modal.css']
+  styleUrls: ['./audiences-page.component.css', '../../css/admin-modal.css']
 })
 export class AudiencesPageComponent {
 
--- src/app/pages/admin/teachers/teachers-page/teachers-page.component.ts
+++ src/app/pages/admin/teachers/teachers-page/teachers-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-teachers-page',
   templateUrl: './teachers-page.component.html',
-  styleUrls: ['../../css/admin-modal.css']
+  styleUrls: ['./teachers-page.component.css', '../../css/admin-modal.css']
 })
 export class TeachersPageComponent {
 
