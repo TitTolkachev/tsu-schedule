1b58936fed5c8342e3e98780744a9865cc0eab48
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-02-23 21:50:00 +0700

    Сделала шаблон шапки для страницы входа

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from "@angular/common/http";
+import { FormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
+import { AppRoutingModule} from "./app-routing.module";
 import { SigninComponent } from './pages/signin/signin.component';
-import {AppRoutingModule} from "./app-routing.module";
 import { TeachersPageComponent } from './pages/admin/teachers/teachers-page/teachers-page.component';
 import { AudiencesPageComponent } from './pages/admin/audiences/audiences-page/audiences-page.component';
 import { SubjectsPageComponent } from './pages/admin/subjects/subjects-page/subjects-page.component';
 import { GroupsPageComponent } from './pages/admin/groups/groups-page/groups-page.component';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
