8000743950846375bfa8e326a48046bda088e9a5
TitTolkachev <tit.tolk07022003@gmail.com>
2023-03-01 21:18:28 +0700

    Слив

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
-
 import {AppRoutingModule} from "./app-routing.module";
-
 import {HeaderSigninComponent} from "./components/headers/header-signin/header-signin.component";
 import {HeaderCompilerPagesComponent} from './components/headers/header-compiler-pages/header-compiler-pages.component';
 import { SignoutComponent } from './pages/signout/signout.component';
@@ -15,8 +13,6 @@ import {SubjectsPageComponent} from './pages/admin/subjects/subjects-page/subjec
 import {GroupsPageComponent} from './pages/admin/groups/groups-page/groups-page.component';
 import {HeaderAdminPagesComponent} from './components/headers/header-admin-pages/header-admin-pages.component';
 import { MainPageComponent } from './pages/admin/main/main-page/main-page.component';
-
-
 import {AppComponent} from './app.component';
 
 @NgModule({
