12f60b0299c937a808d6e98d08ca2e95ea3bbfe1
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-03-04 21:48:11 +0700

    Добавила новую страницу Users для админа

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {AppRoutingModule} from "./app-routing.module";
+
 import {HeaderSigninComponent} from "./components/headers/header-signin/header-signin.component";
 import {HeaderCompilerPagesComponent} from './components/headers/header-compiler-pages/header-compiler-pages.component';
-import { SignoutComponent } from './pages/signout/signout.component';
+import {SignoutComponent} from './pages/signout/signout.component';
 import {SigninComponent} from './pages/signin/signin.component';
 import {TeachersPageComponent} from './pages/admin/teachers/teachers-page/teachers-page.component';
 import {AudiencesPageComponent} from './pages/admin/audiences/audiences-page/audiences-page.component';
 import {SubjectsPageComponent} from './pages/admin/subjects/subjects-page/subjects-page.component';
 import {GroupsPageComponent} from './pages/admin/groups/groups-page/groups-page.component';
 import {HeaderAdminPagesComponent} from './components/headers/header-admin-pages/header-admin-pages.component';
-import { MainPageComponent } from './pages/admin/main/main-page/main-page.component';
+import {MainPageComponent} from './pages/admin/main/main-page/main-page.component';
+import {UsersPageComponent} from './pages/admin/users/users-page/users-page.component';
+
 import {AppComponent} from './app.component';
 
 @NgModule({
@@ -28,7 +31,8 @@ import {AppComponent} from './app.component';
     GroupsPageComponent,
     HeaderSigninComponent,
     HeaderCompilerPagesComponent,
-    HeaderAdminPagesComponent
+    HeaderAdminPagesComponent,
+    UsersPageComponent
   ],
   imports: [
     BrowserModule,
