dd522a535474e3a9c3200eb9c236823877284335
Gerasimchuk Elena <zziikk2003@gmail.com>
2023-03-04 18:13:40 +0700

    Сделала заготовку двух компонент для запросов на странице

--- src/app/pages/admin/requests/requests-page/requests-page.component.ts
+++ src/app/pages/admin/requests/requests-page/requests-page.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {AuthService} from "../../../../services/auth.service";
+import {Router} from "@angular/router";
+import jwtDecode from "jwt-decode";
+import {Jwt} from "../../../../models/jwt";
 
 @Component({
   selector: 'app-requests-page',
@@ -6,5 +10,54 @@ import {Component} from '@angular/core';
   styleUrls: ['./requests-page.component.css']
 })
 export class RequestsPageComponent {
+  state: any
+  login: string = ""
+  password: string = ""
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    this.state = 'loading'
+    if (this.password.length == 0) {
+      this.state = 'password_empty'
+      return
+    }
+    if (this.login.length == 0) {
+      this.state = 'login_empty'
+      return
+    }
+    this.authService.signIn(this.login, this.password)
+      .subscribe({
+        next: e => {
+          let role = jwtDecode<Jwt>(e.token).role
+          if (role == 'Admin') {
+            this.router.navigateByUrl("/admin/main").then()
+            return;
+          }
+          if (role == 'ScheduleWriter') {
+            this.router.navigateByUrl("/moderator/edit").then()
+            return;
+          }
+          this.state = 'forbidden'
+        },
+        error: err => {
+          if (err.status === 0) {
+            this.state = 'connection_refused'
+            return
+          }
+          if (err.error.status === 500) {
+            this.state = 'internal_server_error'
+            return
+          }
+          if (err.error.status === 400) {
+            this.state = 'bad_request'
+            return
+          }
+          this.state = 'unknown_error'
+        }
+      })
+  }
 }
