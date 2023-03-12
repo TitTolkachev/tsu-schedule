99fa57cef2b063eaab8e1a8dd55bb674dbd1e28e
TitTolkachev <tit.tolk07022003@gmail.com>
2023-03-12 17:20:48 +0700

    Пофиксил группы

--- src/app/pages/moderator/edit-page/group-input.ts
+++ src/app/pages/moderator/edit-page/group-input.ts
@@ -3,8 +3,6 @@ export class GroupInput {
     this.init(input, datalist, arrow)
   }
 
-  SelectedGroupsList = []
-
   init(input: HTMLElement | null, datalist: HTMLElement | null, arrow: HTMLElement | null) {
 
     if (input != null && datalist != null) {
@@ -21,40 +19,6 @@ export class GroupInput {
           for (let option of datalist.options)
             option.style.display = "block"
         }
-      }
-      // @ts-ignore
-      for (let option of datalist.options) {
-        option.onclick = function () {
-          if (input != null && datalist != null) {
-
-            // @ts-ignore
-            if (this.SelectedGroupsList.includes(option.value)) {
-              // @ts-ignore
-              this.SelectedGroupsList.splice(this.SelectedGroupsList.indexOf(option.value), 1)
-              option.classList.remove('selected-group-option')
-            } else {
-              // @ts-ignore
-              this.SelectedGroupsList.push(option.value)
-              option.classList.add('selected-group-option')
-            }
-
-            // @ts-ignore
-            this.SelectedGroupsList.sort()
-
-            // @ts-ignore
-            if (this.SelectedGroupsList.length > 0) {
-              // @ts-ignore
-              input.value = this.SelectedGroupsList[0]
-              // @ts-ignore
-              for (let i = 1; i < this.SelectedGroupsList.length; i++) {
-                // @ts-ignore
-                input.value = input.value + ', ' + this.SelectedGroupsList[i]
-              }
-            } else
-              // @ts-ignore
-              input.value = ''
-          }
-        }.bind(this)
       }
     }
 
